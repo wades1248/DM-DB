279eb033936830a7088eb4b23017704220a33ca2
wades1248 <wades1248@gmail.com>
2019-07-16 04:41:37 -0500

    fucked it up

--- src/Generator.js
+++ src/Generator.js
@@ -1,15 +1,14 @@
 import makeCreatures from './Creatures';
 import playerThreshold from './PlayerThreshold';
+import CRXP from './CRXP';
 
 function Generator(params) {
     const Creatures = makeCreatures();
     const {difficulty, environment, creatureNum, players } = params
     const rawCreatures = Creatures;
     const envCreatures =environmentalFilter(environment, rawCreatures);
-    const encounterCreatures = difficultyFilter(difficulty, creatureNum, players, envCreatures);
+    const encounterCreatures = difficultyFilter(difficulty, creatureNum, players, envCreatures, CRXP);
     console.log(encounterCreatures);
-    console.log(creatureNum);
-    
 }
 function environmentalFilter(environment, creatures) {
     if(environment !== 'any'){
@@ -25,41 +24,43 @@ function environmentalFilter(environment, creatures) {
    }
 
 }
-function difficultyFilter(difficulty, creatureNum, players, envCreatures){
+function difficultyFilter(difficulty, creatureNum, players, envCreatures, CRXP){
     const upperLimit =generateUpperDifficulty(difficulty, players)
     const lowerLimit =generateLowerDifficulty(difficulty, players)
     const creatureNumCoefficient = convertCreautureNum(creatureNum);
     const multiply = (a,b,c)=> {return(a*b*c)};
     const evenSplitCreatures = envCreatures.filter(creature => multiply(creature.XP,creatureNum, creatureNumCoefficient) > lowerLimit && multiply(creature.XP, creatureNum, creatureNumCoefficient) < upperLimit)        
-    const oneBigWithXLittle = oneBigXLittle(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures);
+    /*const oneBigWithXLittle = oneBigXLittle(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures);
     const twoBigWithXLittle = twoBigXLittle(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures);
     const oneThirdWithFiller = oneThridFiller(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures);
     const twoThirdWithFiller = twoThridFiller(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures);
-    const AllEncounters = evenSplitCreatures.concat(oneBigWithXLittle, twoBigWithXLittle, oneThirdWithFiller, twoThirdWithFiller)
-    return (AllEncounters);
+    const AllEncounters = evenSplitCreatures.concat(oneBigWithXLittle, twoBigWithXLittle, oneThirdWithFiller, twoThirdWithFiller)*/
+    const q = procedurallyGenerate (lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures,)
+   
+    return q
+
+    
 }
 function generateLowerDifficulty(difficulty, players){
-    if(difficulty === 'easy'){
-        return 0;
-    }else{
+
         const lowerLimitArray = players.map(player => (
             playerThreshold[player.level][difficulty]
         ));
         
         const lowerLimit = lowerLimitArray.reduce(function(total, value){return total+value});
         return(lowerLimit);
-    }
+
 }
 function generateUpperDifficulty(difficulty, players){
-    if(difficulty === 'deadly') {
-        return (1000000)
-    }else{
-        const convertedDifficulty = convertToUpperLimit(difficulty)
+    const convertedDifficulty = convertToUpperLimit(difficulty)
         const upperLimitArray= players.map(player => (
             playerThreshold[player.level][convertedDifficulty]
         ));
-        const lowerLimit = upperLimitArray.reduce(function(total, value){return total+value});
-        return(lowerLimit)
+        const upperLimit = upperLimitArray.reduce(function(total, value){return total+value},0);
+    if(difficulty === 'deadly') {
+        return (2*upperLimit)
+    }else{
+        return(upperLimit)
     }
 }
 function convertToUpperLimit(difficulty) {
@@ -71,7 +72,7 @@ function convertToUpperLimit(difficulty) {
     }else if(difficulty === 'hard'){
         return('deadly')
     }else if(difficulty === 'deadly'){
-        return('hard')
+        return('deadly')
     }
 }
 function convertCreautureNum(creatureNum) {
@@ -89,6 +90,7 @@ function convertCreautureNum(creatureNum) {
         return(4)
     }
 }
+/*
 function oneBigXLittle(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures){
     const half = (a) => {return(a/2)};
     const multiply = (a,b,c)=> {return(a*b*c)};
@@ -214,5 +216,117 @@ function twoThridFiller(lowerLimit, upperLimit, creatureNumCoefficient, creature
         return [];
         };
     
+}*/
+function procedurallyGenerate (lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures){
+    console.log(upperLimit)
+    const countsperCRunfiltered = counterPerCr(upperLimit, creatureNumCoefficient, creatureNum, CRXP)
+    const countsPerCRfiltered = countsPerCRfilter(countsperCRunfiltered)
+    const finalCRarray = generateEncouterCRarray(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, countsPerCRfiltered)
+    console.log(countsPerCRfiltered)
+    return finalCRarray
+}
+function counterPerCr(upperLimit, creatureNumCoefficient, creatureNum, CRXP){
+    let countsperCRunfiltered = []
+    for(let j =1; j <=creatureNum; j++){    
+        for(let i = 0; i < CRXP.length; i++){
+            if((CRXP[i]*creatureNumCoefficient*j) <= upperLimit){
+                countsperCRunfiltered.push( CRXP[i])
+            }
+        }       
+    };
+    return countsperCRunfiltered;
+}
+function countsPerCRfilter(array){
+    const uniqueSet = new Set(array)
+    const uniqueArray= [...uniqueSet]
+    let counts = {};
+    for(let i =0; i < uniqueArray.length; i++){
+            const countItem= uniqueArray[i];
+            const countValue = array.filter(item => item === countItem).length
+            counts[countItem] =countValue;
+        
+    };
+    return counts;
+
+}
+function generateEncouterCRarray(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, envCreatures, CRXP, countsPerCRfiltered){
+    let encounterCRs=[]
+    const CRsArray= Object.keys(countsPerCRfiltered)
+    for(let x= CRsArray.length-1; x>= 0; x--){
+        const CRvalue= CRsArray[x]
+        const calculcatedXP =(CRvalue*(countsPerCRfiltered[CRvalue])*creatureNumCoefficient)
+        if(calculcatedXP >lowerLimit && encounterCRs.length === 0){
+            for(let z = 0; z< countsPerCRfiltered[CRvalue]; z++){
+                encounterCRs.push(parseInt(CRvalue))
+            }
+        };
+        if(encounterCRs.length < creatureNum ){
+            encounterCRs = []
+        }
+        if(x===0 && encounterCRs.length === 0){
+            const bossCRs = makeBossEncounter(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, CRXP, CRsArray);
+            encounterCRs= bossCRs
+        }
+    }
+    return(encounterCRs)
+}
+function makeBossEncounter(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, CRXP, CRsArray){
+    console.log(upperLimit)
+    const boss= CRsArray[(CRsArray.length-1)] 
+    const minionNum= creatureNum-1;
+    const minionUpperLimit= upperLimit- parseInt(boss)
+    const minionLowerLimit= lowerLimit- parseInt(boss)
+    const countsperCRunfiltered = counterPerCr(CRXP, minionUpperLimit, creatureNumCoefficient, minionNum)
+    const countsPerCRfiltered = countsPerCRfilter(countsperCRunfiltered)
+    let encounterCRs=[]
+    const minionsArray= Object.keys(countsPerCRfiltered)
+    for(let x= minionsArray.length-1; x>= 0; x--){
+        const CRvalue= minionsArray[x]
+        const calculcatedXP =(CRvalue*(countsPerCRfiltered[CRvalue])*creatureNumCoefficient)
+        if(calculcatedXP >minionLowerLimit && encounterCRs.length === 0){
+            for(let z = 0; z< countsPerCRfiltered[CRvalue]; z++){
+                encounterCRs.push(parseInt(CRvalue))
+            }
+            encounterCRs.push(parseInt(boss))
+        }
+        if(encounterCRs.length < creatureNum ){
+            encounterCRs = []
+        }
+        if(x===0 && encounterCRs.length === 0){
+            const bossCRs = make2BossEncounter(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum, CRXP);
+            encounterCRs= bossCRs
+        }
+    }
+    return encounterCRs    
+}
+function make2BossEncounter(lowerLimit, upperLimit, creatureNumCoefficient, creatureNum,CRXP, CRsArray){
+    console.log('ruuuuun')
+    const boss1= CRsArray[CRsArray.length-1]
+    const boss2= CRsArray[CRsArray.length-1] 
+    const minionNum= creatureNum-2;
+    const minionUpperLimit= upperLimit- (parseInt(boss1)+parseInt(boss2))
+    const minionLowerLimit= lowerLimit- (parseInt(boss1)+parseInt(boss2))
+    const countsperCRunfiltered = counterPerCr(CRXP, minionUpperLimit, creatureNumCoefficient, minionNum)
+    const countsPerCRfiltered = countsPerCRfilter(countsperCRunfiltered)
+    let encounterCRs=[]
+    const minionsArray= Object.keys(countsPerCRfiltered)
+    for(let x= minionsArray.length-1; x>= 0; x--){
+        const CRvalue= minionsArray[x]
+        const calculcatedXP =(CRvalue*(countsPerCRfiltered[CRvalue])*creatureNumCoefficient)
+        if(calculcatedXP >minionLowerLimit && encounterCRs.length === 0){
+            for(let z = 0; z< countsPerCRfiltered[CRvalue]; z++){
+                encounterCRs.push(parseInt(CRvalue))
+            }
+            encounterCRs.push(parseInt(boss1, boss2))
+        };
+        if(encounterCRs.length < creatureNum ){
+            encounterCRs = []
+        }
+        if(x===0 && encounterCRs.length === 0){
+            //const bossCRs = make2BossEncounter(CRsArray, upperLimit, lowerLimit, creatureNum, creatureNumCoefficient, CRXP,);
+            //encounterCRs= bossCRs
+        }
+    }
+    return encounterCRs    
 }
 export default Generator;
